47f4277a9afa56d47ba655ac52deab01bf61a8c0
zekdot <452526076@qq.com>
2019-06-03 01:39:19 +0800

    修改项目配置路径

--- src/main/java/com/ctrl_i/springboot/service/ArticleService.java
+++ src/main/java/com/ctrl_i/springboot/service/ArticleService.java
@@ -1,12 +1,13 @@
 package com.ctrl_i.springboot.service;
 
+import com.ctrl_i.springboot.ConfigConstant;
 import com.ctrl_i.springboot.dto.Envelope;
 
 /**
  * Create by zekdot on 19-4-13.
  */
 public interface ArticleService {
-    public static String PIC_URL="http://localhost:8081/images/"; //文章的图片URL
+    public static String PIC_URL="http://"+ ConfigConstant.ip+":"+ConfigConstant.fport+ "/images/"; //文章的图片URL
     public static int WORD_LIMIT = 70;    //缩略文字数
     /**
      * 获取文章列表
